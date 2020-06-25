cb3c2061231433d882f4c4764f1d1dff52141fad
Todd Shinto <todd_shinto@yahoo.com>
2020-06-25 22:51:45 +0000

    tables updated and updated select statement for back end pokebox

--- client/components/pokemon-details.jsx
+++ client/components/pokemon-details.jsx
@@ -3,16 +3,22 @@ import React from 'react';
 export default class PokemonDetails extends React.Component {
   render() {
     const pokemon = this.props.pokemon;
-    return (
-      <div>
-        <div>{pokemon.name}</div>
+    if (pokemon) {
+      return (
         <div>
-          <div>{pokemon.species}</div>
-          <div>Type: {pokemon.type}</div>
-          <div>Height: {pokemon.height}</div>
-          <div>Weight: {pokemon.weight}</div>
+          <div>{pokemon.name}</div>
+          <div>
+            <div>{pokemon.species}</div>
+            <div>Type: {pokemon.type}</div>
+            <div>Height: {pokemon.height}</div>
+            <div>Weight: {pokemon.weight}</div>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div>No Pokemon Found</div>
+      );
+    }
   }
 }
