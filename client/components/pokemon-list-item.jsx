312ff4fa8416cb0b45dd2e190b0afda5ce0666ff
Todd Shinto <todd_shinto@yahoo.com>
2020-06-25 18:25:16 +0000

    added set pokemon details through props

--- client/components/pokemon-list-item.jsx
+++ client/components/pokemon-list-item.jsx
@@ -4,7 +4,7 @@ export default class PokemonListItem extends React.Component {
   render() {
     const pokemon = this.props.pokemon;
     return (
-      <div className="pokemon-list-item">
+      <div className="pokemon-list-item" onClick={() => this.props.setPokemonDetails(this.props.key)}>
         <span>{(this.props.key + 1)}. </span>
         {pokemon.name}
       </div>
