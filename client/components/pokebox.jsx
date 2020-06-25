8a8bede5f8ad625d8a45eeae754ac7898c0c35e8
Todd Shinto <todd_shinto@yahoo.com>
2020-06-25 18:10:48 +0000

    added stateful component that gets pokemon from /api/pokeboxes

--- client/components/pokebox.jsx
+++ client/components/pokebox.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
 export default class Pokebox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemon: [],
+      viewDetails: this.state.pokemon[0]
+    };
+    this.getPokemon = this.getPokemon.bind(this);
+  }
+
+  componentDidMount() {
+    this.getPokemon();
+  }
+
+  getPokemon() {
+    fetch('/api/pokeboxes')
+      .then(response => response.json())
+      .then(data => this.setState({ pokemon: data }));
+  }
+
+  setViewDetails(index) {
+    this.setState({ viewDetails: this.state.pokemon[index] });
+  }
+
   render() {
+    const pokemon = this.state.pokemon;
     return (
       <div>
         <h1>Pokebox</h1>
+
         <button onClick={() => this.props.setView('home')} >Home Page</button>
         <button onClick={() => this.props.setView('walk')} >Walk Screen</button>
         <button onClick={() => this.props.setView('backpack')} >Backpack</button>
