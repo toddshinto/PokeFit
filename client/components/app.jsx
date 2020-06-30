6d4320d60628fdbd1936edeee3675a5256e0e576
Todd Shinto <todd_shinto@yahoo.com>
2020-06-30 10:34:35 +0000

    forgot to change start screen back

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -11,7 +11,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: 'walk',
+      view: 'start',
       stats: null,
       pokemons: [],
       items: [],
