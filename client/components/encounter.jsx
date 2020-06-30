99972909657cd05c838db150cb0381c52ea2385b
Todd Shinto <todd_shinto@yahoo.com>
2020-06-30 19:15:19 +0000

    encounter buttons

--- client/components/encounter.jsx
+++ client/components/encounter.jsx
@@ -15,11 +15,21 @@ export default class Encounter extends React.Component {
             <div className="top-screen-second-row">
               <div className="pokemon-encounter" style={{ backgroundImage: `url(${wildPokemon.sprite_front_default})` }} />
             </div>
-
           </div>
         </div>
         <div className="pokedex-screen-container">
-          <div className="pokedex-rectangle-screen stats-walk-screen">
+          <div className="pokedex-rectangle-screen">
+            <div className="encounter-actions">
+              <div className="encounter-button">
+                CATCH
+              </div>
+              <div className="encounter-button">
+                ITEMS
+              </div>
+              <div className="encounter-button">
+                RUN
+              </div>
+            </div>
           </div>
         </div>
       </div>
