eb25efa4130c4cc44e5682bb0b2fe00a6927db2d
Matthewcullen <60274237+Matthewcullen@users.noreply.github.com>
2020-07-01 16:41:38 -0700

    Update footer.jsx

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -2,32 +2,7 @@ import React from 'react';
 
 export default class Footer extends React.Component {
   render() {
-    // let button1;
-    // let button2;
-    // const view = this.props.view;
-    // switch (view) {
-    //   case 'pokebox' :
-    //     button1 = 'backpack';
-    //     button2 = 'walk';
-    //     break;
-    //   case 'walk' :
-    //     button1 = 'backpack';
-    //     button2 = 'pokebox';
-    //     break;
-    //   case 'backpack' :
-    //     button1 = 'pokebox';
-    //     button2 = 'walk';
-    // }
-    // let footer = (
-    //   <div className="pokedex-footer">
-    //     <div className={`${button1}-button-container`} onClick={() => this.props.setView(button1)}>
-    //       <div className={`${button1}-button-icon button-icon`} />
-    //     </div>
-    //     <div className={`${button2}-button-container`} onClick={() => this.props.setView(button2)}>
-    //       <div className={`${button2}-button-icon button-icon`} />
-    //     </div>
-    //   </div>
-    // );
+  
     let footer = (
       <div className="pokedex-footer">
         <div className={'pokebox-button-container'} onClick={() => this.props.setView('pokebox')}>
