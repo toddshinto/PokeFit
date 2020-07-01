94c43c4f0e1edc8c4181f1f854f52779e506804d
Todd Shinto <todd_shinto@yahoo.com>
2020-07-01 23:16:11 +0000

    made all 3 buttons always viewable in footer

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -2,43 +2,53 @@ import React from 'react';
 
 export default class Footer extends React.Component {
   render() {
-    let button1;
-    let button2;
-    const view = this.props.view;
-    switch (view) {
-      case 'pokebox' :
-        button1 = 'backpack';
-        button2 = 'walk';
-        break;
-      case 'walk' :
-        button1 = 'backpack';
-        button2 = 'pokebox';
-        break;
-      case 'backpack' :
-        button1 = 'pokebox';
-        button2 = 'walk';
-    }
-    let footer = (
+    // let button1;
+    // let button2;
+    // const view = this.props.view;
+    // switch (view) {
+    //   case 'pokebox' :
+    //     button1 = 'backpack';
+    //     button2 = 'walk';
+    //     break;
+    //   case 'walk' :
+    //     button1 = 'backpack';
+    //     button2 = 'pokebox';
+    //     break;
+    //   case 'backpack' :
+    //     button1 = 'pokebox';
+    //     button2 = 'walk';
+    // }
+    // let footer = (
+    //   <div className="pokedex-footer">
+    //     <div className={`${button1}-button-container`} onClick={() => this.props.setView(button1)}>
+    //       <div className={`${button1}-button-icon button-icon`} />
+    //     </div>
+    //     <div className={`${button2}-button-container`} onClick={() => this.props.setView(button2)}>
+    //       <div className={`${button2}-button-icon button-icon`} />
+    //     </div>
+    //   </div>
+    // );
+    // if (view === 'encounter') {
+    //   footer = (
+    //     <div className="pokedex-footer">
+    //       <div className="encounter-footer-container">
+    //         <div className="encounter-footer-button" />
+    //       </div>
+    //     </div>
+    //   );
+    // }
+    return (
       <div className="pokedex-footer">
-        <div className={`${button1}-button-container`} onClick={() => this.props.setView(button1)}>
-          <div className={`${button1}-button-icon button-icon`} />
+        <div className={'pokebox-button-container'} onClick={() => this.props.setView('pokebox')}>
+          <div className={'pokebox-button-icon button-icon'} />
         </div>
-        <div className={`${button2}-button-container`} onClick={() => this.props.setView(button2)}>
-          <div className={`${button2}-button-icon button-icon`} />
+        <div className={'backpack-button-container'} onClick={() => this.props.setView('backpack')}>
+          <div className={'backpack-button-icon button-icon'} />
+        </div>
+        <div className={'walk-button-container'} onClick={() => this.props.setView('walk')}>
+          <div className={'walk-button-icon button-icon'} />
         </div>
       </div>
-    );
-    if (view === 'encounter') {
-      footer = (
-        <div className="pokedex-footer">
-          <div className="encounter-footer-container">
-            <div className="encounter-footer-button" />
-          </div>
-        </div>
-      );
-    }
-    return (
-      footer
     );
   }
 }
