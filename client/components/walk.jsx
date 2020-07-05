4893909c17cda1a9831639d5da6740ea47584212
Todd Shinto <todd_shinto@yahoo.com>
2020-07-05 03:54:16 +0000

    time am / pm

--- client/components/walk.jsx
+++ client/components/walk.jsx
@@ -6,10 +6,10 @@ export default class Walk extends React.Component {
     const date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
+    let am = 'AM';
     if (hours < 10) { hours = '0' + hours; }
     if (minutes < 10) { minutes = '0' + minutes; }
-    let s;
-    this.context.stats ? s = this.context.stats : s = { milesWalked: 0, encounters: 0 };
+    if (hours > 12) { hours = hours - 12; am = 'PM'; }
     return (
       <div className="pokedex-body">
         <div className="pokedex-screen-container">
@@ -26,7 +26,7 @@ export default class Walk extends React.Component {
             <div className="stats">
               <div className="stats-text stats-text-walk-screen">
                 <div className="stats-label">MILES WALKED: </div>
-                <div className="stats-number">{s.milesWalked > 0 ? s.milesWalked : 0} </div>
+                <div className="stats-number">{this.context.currMilesWalked.toFixed(2)} </div>
               </div>
               <div className="stats-text stats-text-walk-screen">
                 <div className="stats-label">ENCOUNTERS: </div>
@@ -38,7 +38,7 @@ export default class Walk extends React.Component {
               </div>
               <div className="stats-text stats-text-walk-screen">
                 <div className="stats-label">CURRENT TIME: </div>
-                <div className="stats-number">{hours}:{minutes}</div>
+                <div className="stats-number">{hours}:{minutes}<span style={{ fontSize: '.8em' }}>{am}</span></div>
               </div>
             </div>
           </div>
